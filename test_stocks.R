544988b6786721ade309be4389a6b3867e5a12b0
melofred <frederico.melo@gmail.com>
2015-03-17 19:49:38 -0700

    adding technical indicators

--- test_stocks.R
+++ test_stocks.R
@@ -33,6 +33,19 @@ while(TRUE) {
   # Remove the first X lines (x=3 here)  to avoid NAs
   lag_trainset <- lag_trainset[-1:-3,]
 
+  # include technical indicators
+  ema <- EMA(lag_trainset$LastTradePriceOnly, 5) # 4 first occurences will need to be removed
+  rsi <- RSI(lag_trainset$LastTradePriceOnly, 5)
+  #smi need HLC
+  #sar need HLC
+
+  # use data(ttrc) to generate random data to test
+  # ttrc
+
+  ti = data.frame(ema, rsi)
+  names(ti) <- c("ema","rsi") 
+  
+
   mynet <-neuralnet(Change ~ LastTradePriceOnly + DaysHigh + DaysLow, lag_trainset, hidden = 4, lifesign = "full", linear.output = FALSE, threshold = 0.01)
 
 

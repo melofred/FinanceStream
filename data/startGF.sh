0493e42bffb95d97db2dd72988eedaaf580054ab
Frederico Melo <fmelo@frederimelosmbp.attlocal.net>
2015-04-23 23:02:54 -0700

    working on the docker scripts

--- data/startGF.sh
+++ data/startGF.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#export GF_HOME=/Users/wmarkito/Pivotal/GemFire/sources/github/gemfire/build-artifacts/mac/product
-#export PATH=$GF_HOME/bin:$PATH
 
-gfsh run --file=setup.gfsh
-
+gfsh run --file=data/setup.gfsh
+while true ; do
+   sleep 2
+done
 
 
 

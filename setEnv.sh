23931620006b627841e2ee12fa7a6477fc69a509
Frederico Melo <fmelo@wsip-70-164-161-205.lv.lv.cox.net>
2015-05-08 13:18:06 -0700

    cleaning-up and splitting content in two branches

--- setEnv.sh
+++
@@ -1,2 +0,0 @@
-export STREAM_BASE=`pwd`
-
--- startGF.sh
+++ startGF.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-export GF_HOME=/Users/wmarkito/Pivotal/GemFire/sources/github/gemfire/build-artifacts/mac/product
-export PATH=$GF_HOME/bin:$PATH
-
 gfsh run --file=setup.gfsh
 
 
